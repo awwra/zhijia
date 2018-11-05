3dd2d98c35d52f708f6e2d94ccefe01cd5afcb5e
wuqi <798116520@qq.com>
2018-11-05 09:11:31 +0800

    提交个人中心

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/pages/shouye'
 
+import wode from '@/pages/wode'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path:"/",
       name:'index',
       component:index
-    }
+    },
+    {
+      path:"/wode",
+      name:'wode',
+      component:wode
+    },
 ]
 })
