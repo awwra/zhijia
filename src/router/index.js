6288348791f30727b8e20281772c8f802e61e977
lisong <lisong@aliyun.com>
2018-11-05 21:19:57 +0800

    tijiao

--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/pages/shouye'
-<<<<<<< HEAD
 import shangpinliebiao from '@/pages/shangpinliebiao'
-=======
-<<<<<<< HEAD
-
 import wode from '@/pages/wode'
-=======
 import bottom from '@/components/bottom'
->>>>>>> b7d4e21211200070d9348d54164d07e8aabf314b
->>>>>>> 8d97f3ccde4c60bfcc99e553f8bdc4c873cbef9d
-
+import gouwuche from '@/pages/gouwuche'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path:"/",
-      name:'index',
-      component:index
+  routes: [{
+      path: "/",
+      name: 'index',
+      component: index
     },
     {
-<<<<<<< HEAD
-      path:"/shangpinliebiao",
-      name:'shangpinliebiao',
-      component:shangpinliebiao
+      path: "/gouwuche",
+      name: 'gouwuche',
+      component: gouwuche
     },
-=======
-<<<<<<< HEAD
-      path:"/wode",
-      name:'wode',
-      component:wode
+    {
+      path: "/shangpinliebiao",
+      name: 'shangpinliebiao',
+      component: shangpinliebiao
+    }, {
+      path: "/wode",
+      name: 'wode',
+      component: wode
     },
-=======
-      path:"/bottom",
-      name:'bottom',
-      component:bottom
+    {
+      path: "/bottom",
+      name: 'bottom',
+      component: bottom
     }
->>>>>>> b7d4e21211200070d9348d54164d07e8aabf314b
->>>>>>> 8d97f3ccde4c60bfcc99e553f8bdc4c873cbef9d
-]
+  ]
 })
